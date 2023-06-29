157dfc3ce26809463df8a9bb42f803bbf3cefc53
shankssc <suyashhbk@rediffmail.com>
2023-06-28 20:07:33 -0400

    Setting up UI Kitten

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import globalStyles from './src/styles/globalStyles';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text} from '@ui-kitten/components';
+import {default as theme} from './src/styles/custom-theme-2.json';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={[globalStyles.colorPrimary2, globalStyles.container]}>
+    <NavigationContainer>
+      <Stack.Navigator>
+    <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+    <Layout style={[globalStyles.container]}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Layout>
+    </ApplicationProvider>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,8 +1,8 @@
 import {View, TextInput} from 'react-native';
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 
-const Auth = () => {
+
+const Auth = ({navigation:any}) => {
     
 }
