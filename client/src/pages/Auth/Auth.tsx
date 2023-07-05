91262738b45d3fa90e48e7cd6876a1f3b35bcd00
shankssc <suyashhbk@rediffmail.com>
2023-07-05 19:38:54 -0400

    Stylyzing the Auth form

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,134 +1,102 @@
-import {View, TextInput} from 'react-native';
+import { View, TextInput } from 'react-native';
 import React, { useState } from 'react';
-import { Select, 
-         SelectItem, 
-         Text, 
-         Icon, 
-         IconElement,
-         IndexPath,
-         Layout,
-         Card,
-         Avatar,
-         Toggle,
-         ToggleProps,
-         Input } from '@ui-kitten/components';
+import {
+  Select,
+  SelectItem,
+  Text,
+  Icon,
+  IconElement,
+  IndexPath,
+  Layout,
+  Card,
+  Avatar,
+  Toggle,
+  ToggleProps,
+  Input,
+} from '@ui-kitten/components';
 import styles from './styles';
 
-const Auth = ({navigation:any}) => {
-    
-    const roles = [
-        {label: 'Customer', value: '1'},
-        {label: 'Business owner', value: '2'},
-        {label: 'Carrier', value: '3'},
-        {label: 'Administrator', value: '4'},
-    ]
+const Auth = ({ navigation }: any) => {
+  const roles = [
+    { label: 'Customer', value: '1' },
+    { label: 'Business owner', value: '2' },
+    { label: 'Carrier', value: '3' },
+    { label: 'Administrator', value: '4' },
+  ];
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [isSignup, setIsSignup] = useState(true);
-    const [dropdown, setDropdown] = useState(null);
-    const useToggleState = (initialState = false): ToggleProps => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignup, setIsSignup] = useState(true);
+  const [dropdown, setDropdown] = useState(null);
+
+  const useToggleState = (initialState = false): ToggleProps => {
     const [checked, setChecked] = React.useState(initialState);
-    const [selectedIdx, setSelectedIdx] = React.useState<IndexPath | IndexPath[]>(new IndexPath(0));
 
-
-    const onCheckedChange = (isChecked:any): void => {
-          setChecked(isChecked);
-        };
-      
-        return { checked, onChange: onCheckedChange };
+    const onCheckedChange = (isChecked: any): void => {
+      setChecked(isChecked);
+      setIsSignup(isSignup => !isSignup);
     };
-    
-    const authToggleState = useToggleState();
 
-    const CustIcon = (props:any): IconElement => (
-        <Icon 
-            {...props}
-            name = 'person-outline'
-        />
-    )
+    return { checked, onChange: onCheckedChange };
+  };
 
-    const OwnerIcon = (props:any): IconElement => (
-        <Icon 
-            {...props}
-            name = 'briefcase-outline'
-        />
-    )
+  const authToggleState = useToggleState();
 
-    const CarriIcon = (props:any): IconElement => (
-        <Icon 
-            {...props}
-            name = 'car-outline'
-        />
-    )
+  const CustIcon = (props: any): IconElement => (
+    <Icon {...props} name="person-outline" />
+  );
 
-    const AdminIcon = (props:any): IconElement => (
-        <Icon 
-            {...props}
-            name = 'shield-outline'
-        />
-    )
-    
-    return (
-        <Layout>
-            <Avatar 
-            source={require('../../../assets/images/kotaco.png')}
-            shape='rounded'
-            size='small'
-            style={styles.avatar}
-            />
-            <Card>
-            <Text category="h6">{isSignup?'Sign up': 'Sign in'}</Text>
+  const OwnerIcon = (props: any): IconElement => (
+    <Icon {...props} name="briefcase-outline" />
+  );
 
-            <Toggle 
-                status='info'
-                {...authToggleState}
-            />
+  const CarriIcon = (props: any): IconElement => (
+    <Icon {...props} name="car-outline" />
+  );
 
-            <Input 
-            label = 'username'
-            placeholder = 'Enter a username'
-            />
+  const AdminIcon = (props: any): IconElement => (
+    <Icon {...props} name="shield-outline" />
+  );
 
-            <Input 
-            label = 'email'
-            placeholder = 'Enter an email'
-            />
+  const [selectedIdx, setSelectedIdx] = React.useState<IndexPath | IndexPath[]>(
+    new IndexPath(0)
+  );
 
-            <Input 
-            label = 'password'
-            placeholder = 'Enter a password'
-            />
+  return (
+    <Layout style={styles.container}>
+      <Avatar
+        style={styles.avatar}
+        source={require('../../../assets/images/kotaco.png')}
+        shape="round"
+        size="giant"
+      />
+      <Card style={styles.card}>
+        <Text category="h6">{isSignup ? 'Sign up' : 'Sign in'}</Text>
 
-            <Input 
-            label = 'confirm password'
-            placeholder = 'Repeat your password'
-            />
+        <Toggle status="info" {...authToggleState} />
 
-            <Select
-                label='Role'
-                caption='Please select a role before you submit'
-                selectedIndex={selectedIdx}
-                onSelect={index => setSelectedIdx(index)}
-            >
-                <SelectItem 
-                    title='Customer'
-                    accessoryLeft={CustIcon}
-                />
-                <SelectItem 
-                    title='Owner'
-                    accessoryLeft={OwnerIcon}
-                />
-                <SelectItem 
-                    title='Carrier'
-                    accessoryLeft={CarriIcon}
-                />
-                <SelectItem 
-                    title='Administrator'
-                    accessoryLeft={AdminIcon}
-                />
-            </Select>
+        <Input style={styles.input} label="username" placeholder="Enter a username" />
 
-            </Card>
-        </Layout>
-    )
-}
+        <Input style={styles.input} label="email" placeholder="Enter an email" />
+
+        <Input style={styles.input} label="password" placeholder="Enter a password" />
+
+        <Input style={styles.input} label="confirm password" placeholder="Repeat your password" />
+
+        <Select
+          label="Role"
+          caption="Please select a role before you submit"
+          style={styles.select}
+          selectedIndex={selectedIdx}
+          onSelect={(index) => setSelectedIdx(index)}
+        >
+          <SelectItem title="Customer" accessoryLeft={CustIcon} />
+          <SelectItem title="Owner" accessoryLeft={OwnerIcon} />
+          <SelectItem title="Carrier" accessoryLeft={CarriIcon} />
+          <SelectItem title="Administrator" accessoryLeft={AdminIcon} />
+        </Select>
+      </Card>
+    </Layout>
+  );
+};
+
+export default Auth;
--- client/src/pages/Auth/styles.tsx
+++ client/src/pages/Auth/styles.tsx
@@ -2,7 +2,37 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     avatar: {
+        margin: 12,
         
+    },
+
+    card: {
+        borderRadius: 20,
+        alignItems: 'stretch',
+        overflow: "hidden",
+        marginBottom: 60
+    },
+
+    container: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    input: {
+        width: 250,
+        height: 40,
+        borderWidth: 3,
+        borderRadius: 30,
+        marginTop: 12,
+        marginBottom: 12,
+        paddingBottom: 10
+    },
+
+    select: {
+        marginTop: 12,
+        marginBottom: 12,
+        paddingBottom: 10
     }
 })
 
